3188da97acded0e128462e38bd1bd55d7eed89d6
liefbread <lief@lief-e.net>
2023-05-25 14:03:03 -0400

    TasteFix
    This version should fix the tastes and also adds comments explaining my thought process because each component added.

--- src/components/PotionFlavorProfile.vue
+++ src/components/PotionFlavorProfile.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <p>
-      This potion tastes {{ flavorProfileResult.rating }} with a
-      {{ flavorProfileResult.dominantFlavor }} flavor profile.
+      This potion is {{ flavorProfileResult.rating }} {{flavorProfileResult.conjunction}}
+      {{ flavorProfileResult.dominantFlavor }}.
     </p>
   </div>
 </template>
@@ -12,106 +12,129 @@ import ingredients from "../assets/ingredients.json";
 
 export default {
   name: "PotionFlavorProfile",
+  // Define a prop for the component to receive the potion ingredients
   props: {
     potion: Array,
   },
   data() {
     return {
+      // Load ingredients data from a json file
       ingredients: ingredients.ingredients,
-      flavorRatings: [
-        "Terrible",
-        "Awful",
-        "Palatable",
-        "Decent",
-        "Tasty",
-        "Delicious",
-      ],
+      // Define flavor ratings
+      flavorRatings: ["terrible", "awful", "palatable", "decent", "delicious"],
+      // Define flavor profiles
       flavorProfiles: {
-        umami: ["Tasty", "Saline", "Briny"],
-        spicy: ["Hot", "Burning", "Scorching"],
-        sweet: ["Sugary", "Saccharine", "Cloying"],
-        bitter: ["Dry", "Sharp", "Harsh"],
-        sour: ["Tangy", "Tart", "Acerbic"],
+        umami: ["flavorful", "savory", "rich", "salty", "briny"],
+        heat: ["zingy", "spicy", "hot", "burning", "scorching"],
+        sweet: ["sweet", "sugary", "saccarine", "cloying"],
+        bitter: ["dry", "tannic", "sharp", "acrid", "stringent"],
+        sour: ["tangy", "tart", "biting", "acidic", "acerbic"],
       },
-      potionTaste: { bitter: 0, sour: 0, spicy: 0, sweet: 0, umami: 0 },
-      flavorProfileResult: {}
+      // Initialize taste of the potion
+      potionTaste: { bitter: 0, sour: 0, heat: 0, sweet: 0, umami: 0 },
+      // Initialize the final flavor profile result
+      flavorProfileResult: {},
     };
   },
   methods: {
-    refreshFlavorProfile() {
-      const balance = this.calculateFlavorBalance();
-      const dominantFlavor = this.findDominantFlavor();
-      this.flavorProfileResult = {
-        rating: this.flavorRatings[balance],
-        dominantFlavor: this.flavorProfiles[dominantFlavor][0],
-      };
+    // A method that balances two flavors
+    balanceFlavors(flavor1, flavor2) {
+      let balance = this.potionTaste[flavor1] - this.potionTaste[flavor2];
+      if(balance > 0) {
+        this.potionTaste[flavor1] = balance;
+        this.potionTaste[flavor2] = 0;
+      } else {
+        this.potionTaste[flavor2] = Math.abs(balance);
+        this.potionTaste[flavor1] = 0;
+      }
     },
+// A method that refreshes the flavor profile of the potion
+    refreshFlavorProfile() {
+      // Check if all tastes are 0
+      const isPlain = Object.values(this.potionTaste).every(value => value === 0);
+
+      if (isPlain) {
+        // If all tastes are 0, set the rating to 'plain' and the dominant flavor to 'balanced'
+        this.flavorProfileResult = {
+          rating: 'plain',
+          dominantFlavor: 'balanced',
+          conjunction: 'and'
+        };
+      } else {
+        const balance = this.calculateFlavorBalance();
+        const dominantFlavor = this.findDominantFlavor();
+
+        // Calculate flavor profile index based on the taste intensity
+        let flavorProfileIndex = Math.floor(this.potionTaste[dominantFlavor] / 5);
+
+        // Ensure the index stays within the bounds of the flavorProfiles array
+        flavorProfileIndex = Math.min(flavorProfileIndex, this.flavorProfiles[dominantFlavor].length - 1);
+
+        this.flavorProfileResult = {
+          rating: this.flavorRatings[balance],
+          dominantFlavor: this.flavorProfiles[dominantFlavor][flavorProfileIndex],
+          conjunction: balance <= 1 ? 'but' : 'and'
+        };
+      }
+    },
+    // A method that calculates the flavor balance of the potion
     calculateFlavorBalance() {
+      this.balanceFlavors("umami", "bitter");
+      this.balanceFlavors("heat", "sweet");
+      this.balanceFlavors("bitter", "sweet");
+      this.balanceFlavors("sour", "sweet");
+
+      if(this.potionTaste.sour > this.potionTaste.bitter) this.potionTaste.bitter = 0;
+      if(this.potionTaste.sour > this.potionTaste.heat) this.potionTaste.heat = 0;
+
       let balancedFlavors = 0;
-
-      if (
-        this.potionTaste.bitter >
-        Math.max(this.potionTaste.umami, this.potionTaste.sweet)
-      )
-        balancedFlavors++;
-
-      if (this.potionTaste.umami > this.potionTaste.bitter) balancedFlavors++;
-
-      if (this.potionTaste.sour > this.potionTaste.sweet) balancedFlavors++;
-
-      if (
-        this.potionTaste.spicy >
-        Math.max(this.potionTaste.sweet, this.potionTaste.sour)
-      )
-        balancedFlavors++;
-
-      if (
-        this.potionTaste.sweet >
-        Math.max(
-          this.potionTaste.spicy,
-          this.potionTaste.bitter,
-          this.potionTaste.sour
-        )
-      )
-        balancedFlavors++;
-
-      // Handle Flavor enhancing
-      if (this.potionTaste.sour > 0 && this.potionTaste.umami > 0)
-        this.potionTaste.umami++;
-      if (this.potionTaste.sweet > 0 && this.potionTaste.umami > 0)
-        this.potionTaste.umami++;
+      let flavors = ["bitter", "sour", "heat", "sweet", "umami"];
+      for(let flavor of flavors){
+        if(this.potionTaste[flavor] > 0) balancedFlavors++;
+      }
 
       return balancedFlavors;
     },
+    // A method that finds the dominant flavor of the potion
     findDominantFlavor() {
       return Object.keys(this.potionTaste).reduce((a, b) =>
-        this.potionTaste[a] > this.potionTaste[b] ? a : b
+          this.potionTaste[a] > this.potionTaste[b] ? a : b
       );
     },
+    // A method that combines the taste of all ingredients in the potion
     combineIngredientsTaste() {
       if (!this.potion || !Array.isArray(this.potion)) {
         return;
       }
+      this.potionTaste = { bitter: 0, sour: 0, heat: 0, sweet: 0, umami: 0 };
       for (const ingredientName of this.potion) {
-        const ingredient = this.ingredients.find(
-          (i) => i.name === ingredientName
-        );
+        const ingredient = this.ingredients.find((i) => i.name === ingredientName);
         if (ingredient) {
-          console.log(ingredient.taste)
           this.potionTaste.bitter += ingredient.taste[0];
           this.potionTaste.sour += ingredient.taste[1];
-          this.potionTaste.spicy += ingredient.taste[2];
+          this.potionTaste.heat += ingredient.taste[2];
           this.potionTaste.sweet += ingredient.taste[3];
           this.potionTaste.umami += ingredient.taste[4];
         }
       }
+
+      if(this.potionTaste.sour > 0 && this.potionTaste.umami > 0) this.potionTaste.umami++;
+      if(this.potionTaste.sweet > 0 && this.potionTaste.umami > 0) this.potionTaste.umami++;
     },
   },
+  // When the component is created, combine the ingredients' taste
   created() {
     this.combineIngredientsTaste();
   },
-  updated() {
-    this.refreshFlavorProfile();
-  }
+  // Watch for changes to the potion prop
+  watch: {
+    potion: {
+      immediate: true,
+      handler() {
+        this.combineIngredientsTaste();
+        this.refreshFlavorProfile();
+      },
+    },
+  },
 };
 </script>
